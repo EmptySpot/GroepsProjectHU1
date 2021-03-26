f373ebd14a318a3a457260d96efa4044211a1569
Lobohuargo822 <67827598+Lobohuargo822@users.noreply.github.com>
2021-03-26 14:32:57 +0100

    tester

--- src/code/tester/Main.java
+++ src/code/tester/Main.java
@@ -3,5 +3,6 @@ package code.tester;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Een super mooie tester");
+        System.out.println("Stephan is te laat!");
     }
 }
