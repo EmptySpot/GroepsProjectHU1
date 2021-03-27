a3c04579e6a1d58c46706f387ee194c567838b58
BramPostema <Bram.postema@student.hu.nl>
2021-03-27 15:10:05 +0100

    commit aanwezigheid fixed

--- src/code/tester/Aanwezigheid.java
+++ src/code/tester/Aanwezigheid.java
@@ -7,7 +7,7 @@ public class Aanwezigheid {
     private String extraInformatie;
     private String aanwezig = "aanwezig";
     private OnlineLes onlineLes;
-    private ArrayList<Aanwezigheid>aanwezigheids = new ArrayList<>();
+
 
     public Aanwezigheid(Leerling leerl, String extraI, String aanw, OnlineLes les){
         this.l = leerl;
@@ -15,6 +15,7 @@ public class Aanwezigheid {
         this.aanwezig = aanw;
         this.onlineLes = les;
     }
+
 
 }
 //arraylist van leerlingen lessen reden en aanwezigheid
--- src/code/tester/Leerling.java
+++ src/code/tester/Leerling.java
@@ -1,11 +1,26 @@
 package code.tester;
 
+import com.sun.javafx.collections.ImmutableObservableList;
+
+import java.util.ArrayList;
+
 public class Leerling {
     private int leerlingnummer;
     private String klas;
+    private ArrayList<Aanwezigheid> aanwezigheidlist = new ArrayList<>();
     public Leerling(int lN, String kl){
         this.leerlingnummer = lN;
         this.klas = kl;
+    }
+
+    public void setAanwezigheid(String extraI, String aanw, OnlineLes les){
+        Aanwezigheid a = new Aanwezigheid(this, extraI, aanw, les);
+        aanwezigheidlist.add(a);
+    }
+
+    public ArrayList<Aanwezigheid> getAanwezigheidlist() {
+
+        return ImmutableObservableList(aanwezigheidlist);
     }
 
     public void setLeerlingnummer(int leerlingnummer) {
