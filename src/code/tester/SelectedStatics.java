2cce54dd31644dc9e18d432e40a38ae68079f5c7
Lobohuargo822 <67827598+Lobohuargo822@users.noreply.github.com>
2021-03-28 17:00:32 +0200

    28-mrt-2021 17:00

--- src/code/tester/SelectedStatics.java
+++ src/code/tester/SelectedStatics.java
@@ -1,22 +1,22 @@
-package code.tester;
-
-public class SelectedStatics {
-    private static OnlineLes les;
-    private static Leerling leerling;
-
-    public static void setLes(OnlineLes les){
-        SelectedStatics.les = les;
-    }
-
-    public static OnlineLes getLes(){
-        return les;
-    }
-
-    public static void setLeerling(Leerling leerling){
-        SelectedStatics.leerling = leerling;
-    }
-
-    public static Leerling getLeerling(){
-        return leerling;
-    }
-}
+//package code.tester;
+//
+//public class SelectedStatics {
+//    private static OnlineLes les;
+//    private static Leerling leerling;
+//
+//    public static void setLes(OnlineLes les){
+//        SelectedStatics.les = les;
+//    }
+//
+//    public static OnlineLes getLes(){
+//        return les;
+//    }
+//
+//    public static void setLeerling(Leerling leerling){
+//        SelectedStatics.leerling = leerling;
+//    }
+//
+//    public static Leerling getLeerling(){
+//        return leerling;
+//    }
+//}
--- src/userInterfaceLaag/GeschiedenisController.java
+++ src/userInterfaceLaag/GeschiedenisController.java
@@ -1,3 +1,8 @@
+// deze klasse moet elke keer de afwezigheid van een student laten zien.
+// Waar haalt de klasse die info vandaan?
+// Eerst dummydata nodig?
+// De code moet een student hebben, diens klasseninfo hebben, voor elke les moet-ie doorspitten.
+
 package userInterfaceLaag;
 
 import javafx.fxml.FXMLLoader;
@@ -21,10 +26,17 @@ public class GeschiedenisController {
         mousePressedControle.mousePressedVerwerker(mouseEvent, loader);
     }
 
-
     public void mousePressedUitloggen(MouseEvent mouseEvent) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Inloggen.fxml"));
         mousePressedControle.mousePressedVerwerker(mouseEvent, loader);
     }
 
+
+//    private appAccount account = new account.getHuidigeUser();
+//
+//    public void initialize(){
+//        textField.setText("" + account.getAfwezigheid.size()); // aantal keren afwezig
+
+    }
 }
+
