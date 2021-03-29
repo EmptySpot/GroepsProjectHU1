da270925454ea6695a073ac459fa73aa9ada3e4a
sbrandsen <sbrandsen@mavitec.com>
2021-03-30 00:33:29 +0200

    Cleanup

--- src/userInterfaceLaag/DashboardController.java
+++ src/userInterfaceLaag/DashboardController.java
@@ -1,11 +1,16 @@
 package userInterfaceLaag;
 
+import code.tester.Docent;
+import code.tester.Leerling;
+import code.tester.SelectedStatics;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -13,6 +18,18 @@ import java.io.IOException;
 import java.util.Optional;
 
 public class DashboardController {
+    @FXML
+    private Label persoonNaam;
 
-
+    public void initialize() {
+        if (SelectedStatics.getStatus().equals("Leerling")) {
+            Leerling leerling = (Leerling) SelectedStatics.getPersoon();
+            persoonNaam.setText(leerling.getLeerlingNaam());
+            System.out.println(leerling.getLeerlingnummer());
+        } else if (SelectedStatics.getStatus().equals("Docent")) {
+            Docent docent = (Docent) SelectedStatics.getPersoon();
+            persoonNaam.setText(docent.getDocentNaam());
+            System.out.println(docent.getDocentNaam());
+        }
+    }
 }
