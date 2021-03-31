f54d591b5a11c507312acdf9deb413c1ac5eeef0
Lobohuargo822 <67827598+Lobohuargo822@users.noreply.github.com>
2021-03-31 15:51:50 +0200

    geschiedenis leerling

--- src/userInterfaceLaag/GeschiedenisController.java
+++ src/userInterfaceLaag/GeschiedenisController.java
@@ -1,3 +1,4 @@
+// deze klasse moet elke keer de afwezigheid van een student laten zien. // Waar haalt de klasse die info vandaan? // Eerst dummydata nodig? // De code moet een student hebben, diens klasseninfo hebben, voor elke les moet-ie doorspitten.  package userInterfaceLaag;  import code.tester.*; import javafx.fxml.FXML; import javafx.fxml.FXMLLoader; import javafx.scene.control.Label; import javafx.scene.control.RadioButton; import javafx.scene.control.TextArea; import javafx.scene.control.ToggleGroup; import javafx.scene.input.MouseEvent;  import java.io.IOException; import java.util.List;  public class GeschiedenisController {     @FXML     private Label afwezigCounterLabel;      public void initialize() {         if(SelectedStatics.getStatus().equals("Leerling")){             Leerling leerling = (Leerling) SelectedStatics.getPersoon();             System.out.println(leerling);             List<Aanwezigheid> aanwezigheidslist = leerling.getAanwezigheidlist();             System.out.println(
 // deze klasse moet elke keer de afwezigheid van een student laten zien.
 // Waar haalt de klasse die info vandaan?
 // Eerst dummydata nodig?
@@ -15,32 +16,29 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
+import java.util.List;
 
 public class GeschiedenisController {
     @FXML
     private Label afwezigCounterLabel;
+
     public void initialize() {
-        if (SelectedStatics.getStatus().equals("Leerling")) {
+        if(SelectedStatics.getStatus().equals("Leerling")){
             Leerling leerling = (Leerling) SelectedStatics.getPersoon();
-            int size = leerling.getAanwezigheidlist().size();
-            String size1 = String.valueOf(size);
-            afwezigCounterLabel.setText(size1);
-        } else if (SelectedStatics.getStatus().equals("Docent")) {
-            Docent docent = (Docent) SelectedStatics.getPersoon();
+            System.out.println(leerling);
+            List<Aanwezigheid> aanwezigheidslist = leerling.getAanwezigheidlist();
+            System.out.println(aanwezigheidslist);
+            int afwezigheidCounter = 0;
+            for(Aanwezigheid aanwezigheidInList : aanwezigheidslist) {
+                if(aanwezigheidInList.getAanwezig().equals("Absent")){
+                    System.out.println(aanwezigheidInList);
+                    afwezigheidCounter += 1;
+                }
+            }
+            afwezigCounterLabel.setText("" + afwezigheidCounter);
+        } else if (SelectedStatics.getStatus().equals("Docent")){
+//            Docent docent = (Docent) SelectedStatics.getPersoon();
+
         }
     }
-
-//    public void initialize() {
-//        leerling student = student.getUser();
-//        textField.setText("" + account.getAfwezigheid.size()); // aantal keren afwezig
-//    }
-
-//    Met een for-loop door de aanwezigheid, moet zoeken naar "student" in "rooster" & lessen
-//    Allemaal in de initializer
-//
-//    <fx.id Label.text("Aantal keren afwezig: ") + getAanwezigheid().size();
-
-
-
-    }
-
+}
