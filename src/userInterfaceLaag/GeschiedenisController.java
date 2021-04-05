448a953b1b9cfff0d5a3998b4cf7ffda268e153b
EmptySpot <47001654+EmptySpot@users.noreply.github.com>
2021-04-05 15:54:41 +0200

    Geschiedenis leerlingen

--- src/userInterfaceLaag/GeschiedenisController.java
+++ src/userInterfaceLaag/GeschiedenisController.java
@@ -30,49 +30,20 @@ public class GeschiedenisController {
 
     public void initialize() throws SQLException {
         if (SelectedStatics.getStatus().equals("Leerling")) {
-            Leerling leerling = (Leerling) SelectedStatics.getPersoon();
-            toonLessenGeschiedenis();
-
-            try {
-                ArrayList<Aanwezigheid> test = DatabaseInfo.getAbsentieLeerling(leerling);
-                for (Aanwezigheid AanwezigheidInList : test) {
-                }
-                afwezigCounterLabel.setText("" + test.size());
-            } catch (Exception e) {}
-
-
+            listViewLessen();
         }
     }
-//    SELECT * FROM  aanwezigheid
-//    WHERE aanwezigheid.aanwezigheid = 'Absent' and aanwezigheid.persoonpersoonid = 1 and leslescode = 2
-    public void toonLessenGeschiedenis() throws SQLException {
-        Persoon huidigeGebruiker = SelectedStatics.getPersoon();
-        List<OnlineLes> lessen;
-        if(huidigeGebruiker instanceof  Leerling){
-            DatabaseInfo.getAbsentLessenLeerling();
-            Leerling leerling = (Leerling) huidigeGebruiker;
-            lessen = leerling.getKlas().getLessen();
-            listViewLessen(lessen);
-        }
 
-    }
-
-    public void listViewLessen(List<OnlineLes> lessen) throws SQLException {
-
+    public void listViewLessen() throws SQLException {
         ObservableList<OnlineLes> data = FXCollections.observableArrayList();
-        DatabaseQuerry.setDBConnection();
         Persoon huidigeGebruiker = SelectedStatics.getPersoon();
         Leerling leerling = (Leerling) huidigeGebruiker;
-        Connection connection = DatabaseQuerry.getDBConnection();
-        Statement statement = connection.createStatement();
-        for(OnlineLes les : lessen){
-//            ResultSet resultSet = statement.executeQuery("SELECT * FROM  aanwezigheid\n" +
-//                    "    WHERE aanwezigheid.aanwezigheid = 'Absent' and aanwezigheid.persoonpersoonid = "+leerling+" and leslescode = "+les.getLesCode());
-//            if(resultSet.next()) {
-                data.add(les);}
-            aanwezigheidView.setItems(data);
-            }
-        }
+        ArrayList<OnlineLes> absentlessen = DatabaseInfo.getAbsentieLessen(leerling);
+        data.addAll(absentlessen);
+        aanwezigheidView.setItems(data);
+        afwezigCounterLabel.setText("" + data.size());
+    }
+}
 
 
 
