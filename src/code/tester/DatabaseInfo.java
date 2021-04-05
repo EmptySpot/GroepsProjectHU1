ef325757969129fe31a6c29ac7750bf833853d04
brampostema <Bram.postema@student.hu.nl>
2021-04-05 15:37:56 +0200

    Beveiliging verbeterd

--- src/code/tester/DatabaseInfo.java
+++ src/code/tester/DatabaseInfo.java
@@ -36,7 +36,37 @@ public class DatabaseInfo {
                 klas.updateLes(les, resultSet.getDate(2), resultSet.getTime(3));
             }
         }
+    }public static void getAbsentLessenLeerling() throws SQLException {
+        Leerling leerling = (Leerling) SelectedStatics.getPersoon();
+        Klas klas = leerling.getKlas();
+
+        Connection connection = DatabaseQuerry.getDBConnection();
+
+        Statement statement = connection.createStatement();
+        Statement statement2 = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("SELECT aanwezigheid.persoonpersoonid,aanwezigheid.leslescode,aanwezigheid.extrainformatie,aanwezigheid.aanwezigheid from aanwezigheid, les WHERE klasklasnaam = '" + klas + "' AND aanwezigheid.persoonpersoonid= "+leerling+" ORDER BY time ASC");
+
+        while (resultSet.next()) {
+            ResultSet docentSet = statement2.executeQuery("SELECT * from persoon WHERE persoonid = '" + resultSet.getString(7) + "'");
+            docentSet.next();
+
+            OnlineLes les = klas.getLes(resultSet.getString(1));
+            String docentid = resultSet.getString(7);
+            Docent docent = School.getDocent(docentid);
+            if (docent == null) {
+                docent = new Docent(docentSet.getString(2), docentSet.getString(1));
+            }
+
+            if (les == null) {
+                new OnlineLes(resultSet.getDate(2), resultSet.getString(1), resultSet.getBoolean(4), resultSet.getString(5), resultSet.getString(6), klas, docent, resultSet.getTime(3),resultSet.getString(9));
+            } else if (!SelectedStatics.controleLes(les, resultSet.getDate(2), resultSet.getTime(3))) {
+                klas.updateLes(les, resultSet.getDate(2), resultSet.getTime(3));
+            }
+        }
     }
+//    ResultSet resultSet = statement.executeQuery("SELECT * FROM  aanwezigheid\n" +
+//            "    WHERE aanwezigheid.aanwezigheid = 'Absent' and aanwezigheid.persoonpersoonid = "+leerling+" and leslescode = "+les.getLesCode());
 
     public static void getLessenDocent() throws SQLException {
         Docent docent = (Docent) SelectedStatics.getPersoon();
