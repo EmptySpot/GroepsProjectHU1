18af3b2edbe06eb50648d0c4f2eac16eb050395a
Lobohuargo822 <67827598+Lobohuargo822@users.noreply.github.com>
2021-04-06 12:43:30 +0200

    HUlp pagina

--- src/userInterfaceLaag/HulpPaginaController.java
+++ src/userInterfaceLaag/HulpPaginaController.java
@@ -1,10 +1,29 @@
 package userInterfaceLaag;
 
+
+import javafx.scene.control.Hyperlink;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class HulpPaginaController {
 
 
     public void extraHulp(){
+        if(Desktop.isDesktopSupported())
+        {
+            try {
+                Desktop.getDesktop().browse(new URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            } catch (URISyntaxException e1) {
+                e1.printStackTrace();
+            }
+        }
 
-        String URL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        }
     }
-}
+
+
