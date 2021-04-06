28a6403f7d421929c1bfd63f9077c71d7cf0849f
brampostema <Bram.postema@student.hu.nl>
2021-04-06 13:02:15 +0200

    alles zo goed als af

--- src/userInterfaceLaag/AdminDashboardController.java
+++ src/userInterfaceLaag/AdminDashboardController.java
@@ -6,21 +6,21 @@ import javafx.scene.input.MouseEvent;
 import java.io.IOException;
 
 public class AdminDashboardController {
-    public void docentAanmakenButton(MouseEvent mouseEvent) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("DocentAanmaken.fxml"));
-        mousePressedControle.mousePressedVerwerker(mouseEvent, loader);
-    }
-    public void wachtwoordOpvragenButton(MouseEvent mouseEvent) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("WachtwoordOpvragen.fxml"));
-        mousePressedControle.mousePressedVerwerker(mouseEvent, loader);
-    }
-    public void BlokkeerAttemptsButton(MouseEvent mouseEvent) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("BlokkeerAttempts.fxml"));
-        mousePressedControle.mousePressedVerwerker(mouseEvent, loader);
-    }
-    public void deblokkerenButton(MouseEvent mouseEvent) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Beveiliging.fxml"));
-        mousePressedControle.mousePressedVerwerker(mouseEvent, loader);
-    }
+//    public void docentAanmakenButton(MouseEvent mouseEvent) throws IOException {
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("DocentAanmaken.fxml"));
+//        mousePressedControle.mousePressedVerwerker(mouseEvent, loader);
+//    }
+//    public void wachtwoordOpvragenButton(MouseEvent mouseEvent) throws IOException {
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("WachtwoordOpvragen.fxml"));
+//        mousePressedControle.mousePressedVerwerker(mouseEvent, loader);
+//    }
+//    public void BlokkeerAttemptsButton(MouseEvent mouseEvent) throws IOException {
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("BlokkeerAttempts.fxml"));
+//        mousePressedControle.mousePressedVerwerker(mouseEvent, loader);
+//    }
+//    public void deblokkerenButton(MouseEvent mouseEvent) throws IOException {
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("Beveiliging.fxml"));
+//        mousePressedControle.mousePressedVerwerker(mouseEvent, loader);
+//    }
 
 }
